203fbd2af9a2f3074f80c582e97f0f840f6ec23a
Trevor Hillebrand <trhille@uw.edu>
2019-01-28 12:49:39 -0500

    Further fixes to instances of passing scaling_w and scaling_e to calc_h_0.m

--- run_min_search_E_and_fs_xP.m
+++ run_min_search_E_and_fs_xP.m
@@ -17,8 +17,8 @@ N_fs          = length( fs_vec );
 % Might be working to run through both together but don't have file saving
 % setup and don't have it double checked. 
 
-for iterations = 1   % Minimize for both Darwin (1) and Hatherton (2)
-%  for iterations = 2    
+% for iterations = 1   % Minimize for both Darwin (1) and Hatherton (2)
+ for iterations = 2    
     
     if (iterations == 1)
         disp('Running for Darwin...')
@@ -345,7 +345,9 @@ RMS_mismatch_matrix2 = NaN * ones( N_E, N_fs );
                                          A_eff_edges_xt2(time2,1:end-1), ...
                                          A_eff_edges_xt2(time2,2:end), ...
                                          flux_add_w2, flux_add_e2, ...
-                                         deformation_only2, deformation_plus_sliding2, sliding_only2);
+                                         deformation_only2,...
+                                         deformation_plus_sliding2, sliding_only2,...
+                                         scaling_w2, scaling_e2);
     
                                      
 % edge values of ice thickness
@@ -397,8 +399,8 @@ RMS_mismatch_matrix2 = NaN * ones( N_E, N_fs );
  % % Both:
  %   residual2 = sqrt( mean( ( (abs(Hat_velocity) - abs(surf_vel_estimate2))/std_dev ).^2 ) ) + ...
  %            sqrt( mean( ( (abs(Hat_surface) - abs(S_P2(1,:)))/std_dev ).^2 ) );    
-   residual2 = sqrt( mean( ( (abs(Hat_velocity(xpos2)) - abs(surf_vel_estimate2(xpos2)))/std_dev_vel(xpos) ).^2 ) ) + ...
-              sqrt( mean( ( (abs(Hat_surface(xpos2)) - abs(S_P2(1,xpos2)))/std_dev_surface(xpos) ).^2 ) );   
+   residual2 = sqrt( mean( ( (abs(Hat_velocity(xpos2)) - abs(surf_vel_estimate2(xpos2)))/std_dev_vel(xpos2) ).^2 ) ) + ...
+              sqrt( mean( ( (abs(Hat_surface(xpos2)) - abs(S_P2(1,xpos2)))/std_dev_surface(xpos2) ).^2 ) );   
           
   
   
@@ -491,7 +493,9 @@ end  % loop over xpositions
                                          A_eff_edges_xt2(time2,1:end-1), ...
                                          A_eff_edges_xt2(time2,2:end), ...
                                          flux_add_w2, flux_add_e2, ...
-                                         deformation_only2, deformation_plus_sliding2, sliding_only2);
+                                         deformation_only2,...
+                                         deformation_plus_sliding2, sliding_only2,...
+                                         scaling_w2, scaling_e2);
               
 % Find the flux
 % ==============
